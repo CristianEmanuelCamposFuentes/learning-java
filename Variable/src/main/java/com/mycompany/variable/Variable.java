19d6625d5a4e99b6462fb7a3be7dee7001a5650a
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-05-21 21:35:36 -0300

    Inicio de proyecto

---
+++ Variable/src/main/java/com/mycompany/variable/Variable.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variable;
+
+/**
+ *
+ * @author crist
+ */
+public class Variable {
+
+    public static void main(String[] args) {
+        int num1, num2, resultado;
+     
+        num1 = 4;
+        num2 = 2;
+        
+        resultado = num1 + num2;
+        System.out.println("El resultado de la suma es: " + resultado);
+        
+    }
+}
