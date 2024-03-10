ac7eb328c9533cb1f55ce4d03c60118032fc48f1
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-03-09 21:53:58 -0300

    Create PrimitivosBoolean and SistemasNumericos class

--- HolaMundo/src/SistemasNumericos.java
+++ HolaMundo/src/SistemasNumericos.java
@@ -8,19 +8,28 @@ public class SistemasNumericos {
         System.out.println("Numero decimal: " + numeroDecimal);
 
         // Convertir a binario
-        System.out.println("Numero binario: " + Integer.toBinaryString(numeroDecimal));
+        String mensajeBinario = "Numero binario de " + numeroDecimal + " es: " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(mensajeBinario);
 
         int numeroBinario = 0b111110100;
         System.out.println(numeroBinario);
 
         // Convertir a octal
-        System.out.println("Numero octal de " + numeroDecimal + " es: " + Integer.toOctalString(numeroDecimal));
+        String mensajeOctal = "Numero octal de " + numeroDecimal + " es: " + Integer.toOctalString(numeroDecimal);
+        System.out.println(mensajeOctal);
         int numeroOctal = 036;
         System.out.println("Numero octal: " + numeroOctal);
 
         // Convertir a hexadecimal
-        System.out.println("Numero hexadecimal de: " + numeroDecimal + " es: " + Integer.toHexString(numeroDecimal));
+        String mensajeHexadecimal = "Numero hexadecimal de: " + numeroDecimal + " es: " + Integer.toHexString(numeroDecimal);
+        System.out.println(mensajeHexadecimal);
         int numeroHexadecimal = 0x1e;
         System.out.println("Numero hexadecimal: " + numeroHexadecimal);
+
+        String mensaje = mensajeBinario;
+        mensaje = mensaje + "\n" + mensajeOctal;
+        mensaje = mensaje + "\n" + mensajeHexadecimal;
+        JOptionPane.showMessageDialog(null, mensaje);
+        JOptionPane.showMessageDialog(null, "Programa terminado");
     }
 }
