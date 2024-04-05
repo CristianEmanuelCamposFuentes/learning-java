2e13f18c02ddeaec9322d9ef19bfae10688c00a7
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-04-04 23:46:56 -0300

    Create sort method

--- src/Arreglos/EjemploArreglosForOrdenamientoBurbuja.java
+++ src/Arreglos/EjemploArreglosForOrdenamientoBurbuja.java
@@ -17,28 +17,30 @@ public class EjemploArreglosForOrdenamientoBurbuja {
             total2--;
         }
     }
+
+    public static void  sortBurbuja(Object[] arreglo) {
+        int total = arreglo.length;
+        int contador = 0;
+        for (int i = 0; i < total- 1; i++) {
+
+            for (int j = 0; j < total - 1 - i; j++) {
+                if ( ((Comparable) arreglo[j + 1]).compareTo(arreglo[j]) < 0) {
+                    Object aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                }
+                contador++;
+            }
+        }
+        System.out.println("Total de comparaciones: " + contador);
+    }
     public static void main(String[] args) {
 
         String[] productos = {"Kingston Pendrive 64GB", "Samsung Galaxy", "Disco SSD 1 TB", "Asus Notebook", "Macbook Air", "Chromecast", "Bicicleta Oxford"};
 
         int total = productos.length;
-        int contador = 0;
 
-
-// Van a ser dos For, el primero recorre cada elemento del arreglo, y el segundo los compara cada elemento con cada elemento del array
-        for (int i = 0; i < total - 1; i++) {
-            for (int j = 0; j < total - 1 - i; j++) {
-                if (productos[j + 1].compareTo(productos[j]) < 0) {
-                    // El auxiliar se necesita para intercambiar valores, manteniendo el valor de la variable de iteracion
-                    String aux = productos[j];
-                    productos[j] = productos[j+1 ];
-                    productos[j+1] = aux;
-                }
-                contador++;
-            }
-        }
-
-        System.out.println("Total de comparaciones: " + contador);
+        sortBurbuja(productos);
         //Arrays.sort(productos);
         //arregloInverso(productos);
 
@@ -49,6 +51,19 @@ public class EjemploArreglosForOrdenamientoBurbuja {
         System.out.println("=== Usando For ===");
         for (int i = 0; i < productos.length; i++) {
             System.out.println("Producto " + (i + 1) + ": " + productos[i]);
+        }
+
+
+        Integer[] numeros = new Integer[4];
+        numeros[0] = 10;
+        numeros[1] = Integer.valueOf("7");
+        numeros[2] = 30;
+        numeros[3] = -1;
+
+        sortBurbuja((numeros));
+        System.out.println("=== Usando For con enteros ===");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Numero " + (i + 1) + ": " + numeros[i]);
         }
     }
 }
