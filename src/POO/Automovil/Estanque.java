cb004530c801473123e394f9e5eed94930bd82c8
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-04-28 20:33:46 -0300

    Create Estanque.java

---
+++ src/POO/Automovil/Estanque.java
@@ -0,0 +1,16 @@
+package POO.Automovil;
+
+public class Estanque {
+    private int capacidad;
+
+    public Estanque() {
+    }
+
+    public Estanque(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+}
