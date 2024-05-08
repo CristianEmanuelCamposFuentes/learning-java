58fd57bf199ca159cb46139cb45b1120417f78f4
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-05-07 21:43:45 -0300

    Create Calculadora.java

--- src/POO/Sobrecarga/Calculadora.java
+++ src/POO/Sobrecarga/Calculadora.java
@@ -1,26 +1,29 @@
 package POO.Sobrecarga;
 
 public class Calculadora {
-    public int sumar(int a, int b){
+    private Calculadora(){
+
+    }
+    public static int sumar(int a, int b){
         return a+b;
     }
-    public float sumar(float x, float y){
+    public static float sumar(float x, float y){
         return x+y;
     }
 
-    public float sumar(int i,float j){
+    public static float sumar(int i,float j){
         return i+j;
     }
 
-    public float sumar(float i, int j){
+    public static float sumar(float i, int j){
         return i+j;
     }
 
-    public double sumar(double i, double j){
+    public static double sumar(double i, double j){
         return i+j;
     }
 
-    public int sumar(String a, String b){
+    public static int sumar(String a, String b){
         int resultado;
         try {
             resultado = Integer.parseInt(a) + Integer.parseInt(b);
@@ -30,7 +33,24 @@ public class Calculadora {
         return resultado;
     }
 
-    public int sumar(int a, int b, int c){
+    public static int sumar(int a, int b, int c){
         return a+b+c;
+    }
+
+    // Numero variable de argumentos
+    public static int sumar(int... args){
+        int resultado = 0;
+        for (int i = 0; i < args.length; i++) {
+            resultado += args[i];
+        }
+        return resultado;
+    }
+
+    public static long sumar(long a, int... args){
+        long resultado = a;
+        for (int i = 0; i < args.length; i++) {
+            resultado += args[i];
+        }
+        return resultado;
     }
 }
--- src/POO/Sobrecarga/EjemploSobrecarga.java
+++ src/POO/Sobrecarga/EjemploSobrecarga.java
@@ -1,16 +1,17 @@
 package POO.Sobrecarga;
-
+import static POO.Sobrecarga.Calculadora.*;
 public class EjemploSobrecarga {
 
     public static void main(String[] args) {
+        System.out.println("Sumar 2 enteros: " + sumar(1, 2));
+        System.out.println("Sumar 2 enteros: " + sumar(1, 2, 3));
+        System.out.println("Sumar 2 doubles: " + sumar(1.2, 2.0));
+        System.out.println("Sumar 2 doubles: " + sumar(1.3, 2.3));
+        System.out.println("Sumar dos flotantes: " + sumar(3.4f, 5.6f));
+        System.out.println("Sumar strings: " + sumar("Hola ", "Mundo"));
+        System.out.println("Sumar longs: " + sumar(3L, 5L));
 
-        Calculadora calculadora = new Calculadora();
-        System.out.println("Sumar 2 enteros: " + calculadora.sumar(1, 2));
-        System.out.println("Sumar 2 enteros: " + calculadora.sumar(1, 2, 3));
-        System.out.println("Sumar 2 doubles: " + calculadora.sumar(1.2, 2.0));
-        System.out.println("Sumar 2 doubles: " + calculadora.sumar(1.3, 2.3));
-        System.out.println("Sumar dos flotantes: " + calculadora.sumar(3.4f, 5.6f));
-        System.out.println("Sumar strings: " + calculadora.sumar("Hola ", "Mundo"));
-        System.out.println("Sumar longs: " + calculadora.sumar(3L, 5L));
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("Sumar array de enteros: " + sumar(array));
     }
 }
