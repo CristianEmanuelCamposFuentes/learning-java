e80f03558bd645faea3e1ff491408fe9f32d3939
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-05-08 22:21:55 -0300

    Create EjemploHerencia.java

--- src/POO/Herencia/Alumno.java
+++ src/POO/Herencia/Alumno.java
@@ -6,6 +6,31 @@ public class Alumno extends Persona {
     private double notaLenguaje;
     private double notaIngles;
 
+    public Alumno() {
+        System.out.println("Alumno: Inicializando constructor...");
+    }
+
+    public Alumno(String nombre, String apellido){
+        super(nombre, apellido);
+    }
+
+    public Alumno(String nombre, String apellido, int edad){
+        super(nombre, apellido, edad);
+    }
+
+    public Alumno(String nombre, String apellido, int edad, String institucion){
+        super(nombre, apellido, edad);
+        this.institucion = institucion;
+    }
+
+    public Alumno(String nombre, String apellido, int edad, String institucion, double notaMatematicas, double notaLenguaje, double notaIngles){
+        this(nombre, apellido, edad, institucion);
+        this.notaMatematicas = notaMatematicas;
+        this.notaLenguaje = notaLenguaje;
+        this.notaIngles = notaIngles;
+    }
+
+
     public String getInstitucion() {
         return institucion;
     }
--- src/POO/Herencia/Profesor.java
+++ src/POO/Herencia/Profesor.java
@@ -3,6 +3,23 @@ package POO.Herencia;
 public class Profesor extends Persona {
     private String materia;
 
+    public Profesor() {
+        System.out.println("Profesor: Inicializando constructor");
+    }
+
+    public Profesor(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+    public Profesor(String nombre, String apellido, int edad) {
+        super(nombre, apellido, edad);
+    }
+
+    public Profesor(String nombre, String apellido, int edad, String materia) {
+        super(nombre, apellido, edad);
+        this.materia = materia;
+    }
+
     public String getMateria() {
         return materia;
     }
