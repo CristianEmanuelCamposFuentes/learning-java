6af0ca6992cc459d1a68f00bb9763bda4b421a16
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-06-17 17:56:26 -0300

    Create RequeridoValidador.java

---
+++ src/POO/ClasesAbstractas/Form/validador/RequeridoValidador.java
@@ -0,0 +1,20 @@
+package POO.ClasesAbstractas.Form.validador;
+
+public class RequeridoValidador extends Validador{
+
+    protected String mensaje = "El campo es requerido";
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return (valor != null && !valor.isBlank());
+    }
+}
