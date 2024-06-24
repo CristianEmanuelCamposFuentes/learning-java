8888ab2d411d7210ac1a3a853df4f2f32d522974
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-06-24 17:10:56 -0300

    Add listar methods

--- src/POO/Interfaces/CrudRepositorio/repositorio/ClienteListRepositorio.java
+++ src/POO/Interfaces/CrudRepositorio/repositorio/ClienteListRepositorio.java
@@ -3,6 +3,7 @@ package POO.Interfaces.CrudRepositorio.repositorio;
 import POO.Interfaces.CrudRepositorio.modelo.Cliente;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ClienteListRepositorio implements CrudRepositorio,
@@ -22,8 +23,8 @@ public class ClienteListRepositorio implements CrudRepositorio,
     @Override
     public Cliente porId(Integer id) {
         Cliente resultado = null;
-        for (Cliente cli: dataSource) {
-            if(cli.getId().equals(id)) {
+        for (Cliente cli : dataSource) {
+            if (cli.getId().equals(id)) {
                 resultado = cli;
                 break;
             }
@@ -48,14 +49,32 @@ public class ClienteListRepositorio implements CrudRepositorio,
         cli.setApellido(cliente.getApellido());
     }
 
+
     @Override
     public List<Cliente> listar(String campo, Direccion dir) {
-        return List.of();
+        dataSource.sort(( a,  b) -> {
+                int resultado = 0;
+                if (dir == Direccion.ASCENDENTE) {
+                    switch (campo) {
+                        case "id" -> resultado = a.getId().compareTo(b.getId());
+                        case "nombre" -> resultado = a.getNombre().compareTo(b.getNombre());
+                        case "apellido" -> resultado = a.getApellido().compareTo(b.getApellido());
+                    }
+                } else if (dir == Direccion.DESCENDENTE) {
+                    switch (campo) {
+                        case "id" -> resultado = b.getId().compareTo(a.getId());
+                        case "nombre" -> resultado = b.getNombre().compareTo(a.getNombre());
+                        case "apellido" -> resultado = b.getApellido().compareTo(a.getApellido());
+                    }
+                }
+                return resultado;
+        });
+        return dataSource;
     }
 
     @Override
     public List<Cliente> listar(int desde, int hasta) {
-        return List.of();
+        return dataSource.subList(desde, hasta);
     }
 
 
