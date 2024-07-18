9243d2788d397e06cb1f377ebeb3c31c24c9ac5a
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-07-18 20:42:21 -0300

    Create LecturaAccesoDatoException.java

---
+++ src/POO/Interfaces/CrudRepositorio/repositorio/excepciones/LecturaAccesoDatoException.java
@@ -0,0 +1,7 @@
+package POO.Interfaces.CrudRepositorio.repositorio.excepciones;
+
+public class LecturaAccesoDatoException extends AccesoDatoException {
+    public LecturaAccesoDatoException(String message) {
+        super(message);
+    }
+}
