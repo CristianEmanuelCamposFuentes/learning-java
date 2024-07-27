87f1d03516714cc075e94eac7c3d014a56ba6b67
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-07-26 21:12:35 -0300

    Create EjemploTreeSetComparable.java

--- src/Collections/Ejemplos/set/EjemploTreeSetComparable.java
+++ src/Collections/Ejemplos/set/EjemploTreeSetComparable.java
@@ -1,15 +1,13 @@
 package Collections.Ejemplos.set;
 
 import Collections.Ejemplos.modelo.Alumno;
-
-import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class EjemploTreeSetComparable {
     public static void main(String[] args) {
 
-        Set<Alumno> sa = new TreeSet<>();
+        Set<Alumno> sa = new TreeSet<>((a,b) -> b.getNota().compareTo(a.getNota()));
         sa.add(new Alumno("Cristian", 8));
         sa.add(new Alumno("Cintia", 6));
         sa.add(new Alumno("Emanuel", 9));
