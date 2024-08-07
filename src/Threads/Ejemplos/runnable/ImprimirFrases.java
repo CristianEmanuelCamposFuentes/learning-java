61e7383b5e787c64768925c476c42c1980b944e4
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-08-06 23:26:10 -0300

    Create ImprimirFrases.java

---
+++ src/Threads/Ejemplos/runnable/ImprimirFrases.java
@@ -0,0 +1,21 @@
+package Threads.Ejemplos.runnable;
+
+import Threads.Ejemplos.EjemploSincronizacionThread;
+
+import static Threads.Ejemplos.EjemploSincronizacionThread.imprimirFrases;
+
+public class ImprimirFrases implements Runnable {
+
+    private String frase1, frase2;
+
+    public ImprimirFrases(String frase1, String frase2) {
+        this.frase1 = frase1;
+        this.frase2 = frase2;
+    }
+
+    @Override
+    public void run() {
+        imprimirFrases(this.frase1,this.frase2);
+
+    }
+}
